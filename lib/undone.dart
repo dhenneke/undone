9ba81cccc2cd0dace1016028439465b3b295b802
rmsmith <ross.dart.lang@gmail.com>
2013-03-18 13:35:15 -0700

    doc tweak

--- lib/undone.dart
+++ lib/undone.dart
@@ -178,16 +178,16 @@ class Transaction extends Action {
 
 /// An asynchronous schedule of actions.
 ///
-/// A schedule is a [Function] that can be [call]ed with a given action.  The
-/// order of such calls is preserved in a history to allow for [undo] and 
-/// [redo].  An action may be scheduled at any time; if the schedule is not
-/// [busy] then it will be called immediately, otherwise it will be queued to be
-/// called as soon as possible.  Methods to change the history such as [undo]
-/// and [redo] can _not_ be invoked when the schedule is [busy].  This ensures
-/// that all queued actions are called and the schedule reaches an idle state
-/// before the history may be modified.  The schedule is a state machine, and
-/// its [states] are observable as a stream; this provides a convenient means to
-/// connect a user interface to the history control methods.
+/// A schedule is a [Function] that can be [call]ed with [Action]s.  The order 
+/// of such calls is preserved in a history to allow for [undo] and [redo].  An 
+/// action may be scheduled at any time; if the schedule is not [busy] then it 
+/// will be called immediately, otherwise it will be queued to be called as soon 
+/// as possible.  Methods to change the history such as [undo] and [redo] can 
+/// _not_ be invoked when the schedule is [busy].  This ensures that all queued 
+/// actions are called and the schedule reaches an idle state before the history 
+/// may be modified.  The schedule is a state machine, and its [states] are 
+/// observable as a stream; this provides a convenient means to connect a user 
+/// interface to the history control methods.
 class Schedule {
   /// A schedule is idle (not busy).
   static const int STATE_IDLE = 0;
