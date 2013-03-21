b51b43e71fbcdf6ee9a00e098c2495335697ad34
rmsmith <ross.dart.lang@gmail.com>
2013-03-20 19:58:51 -0700

    README: added transaction and made the undo/redo bindings more real world

--- README.md
+++ README.md
@@ -32,16 +32,29 @@ action().then((result) => print('$result')); // prints '43'
 ### Undo and Redo
 
 ```dart
-// Undo your action.
-undo().then((_) => print('${map["value"]}')); // prints '42'
-
-// Redo your action.
-redo().then((_) => print('${map["value"]}')); // prints '43'
+// Bind undo / redo to keyboard events.
+document.onKeyUp.listen((e) {    
+  if (e.ctrlKey) {
+    if (e.keyCode == KeyCode.Z)           undo();
+    else if (e.keyCode == KeyCode.Y)      redo();
+  }
+});
 ```
 
 ### Group Actions in a Transaction
 
-TODO:
+```dart
+// Define another action to determine the square of the map's 'value'
+Do square = (a) => a['value'] = a['value'] * a['value'];
+Undo squareRoot = (a, _) => a['value'] = math.sqrt(a['value']);
+var action2 = new Action(map, square, squareRoot);
+  
+// Schedule a transaction that contains both actions (increment then square).
+transact(() {
+    action();
+    action2();
+}).then((_) => print('${map["value"]}')); // prints '1936'
+```
 
 _Undone uses the MIT license as described in the LICENSE file, and follows
 [semantic versioning][]._
--- example/readme.dart
+++ example/readme.dart
@@ -2,6 +2,7 @@
 // This library contains the example code used in the README file.
 library readme;
 
+import 'dart:math' as math;
 import 'package:undone/undone.dart';
 
 main() {  
@@ -27,29 +28,25 @@ main() {
   
   // Call your action, and listen for the result (if you want) - its easy!
   action().then((result) => print('$result')); // prints '43'
+  
+  //----------------------------------------------------------------------------
+  // Group Actions in a Transaction.
+  //----------------------------------------------------------------------------
     
+  // Define another action to determine the square of the map's 'value'
+  Do square = (a) => a['value'] = a['value'] * a['value'];
+  Undo squareRoot = (a, _) => a['value'] = math.sqrt(a['value']);
+  var action2 = new Action(map, square, squareRoot);
+  
+  // Schedule a transaction that contains both actions (increment then square).
+  transact(() {
+      action();
+      action2();
+  }).then((_) => print('${map["value"]}')); // prints '1936'
+  
   //----------------------------------------------------------------------------
   // Undo and Redo.
   //----------------------------------------------------------------------------  
   
-  // Run undo / redo in a sequence of steps.
-  // In a real application you will typically bind user interface controls to
-  // undo and redo, and those controls will be enabled only in STATE_IDLE.  
-  // See our 'nudge' example for a more real-world usage.
-  int step = 0;  
-  schedule.states.listen((state) {
-    if (state == Schedule.STATE_IDLE) {
-      switch(step) {
-        case 0:
-          // Undo your action.
-          undo().then((_) => print('${map["value"]}')); // prints '42'
-          break;
-        case 1: 
-          // Redo your action.
-          redo().then((_) => print('${map["value"]}')); // prints '43'
-          break;
-      }
-      step++;
-    }
-  });
+  // See 'nudge.dart' for undo and redo bindings.  
 }
