03657767dc22ae56eca155c96dd93fea8e84d17f
rmsmith <dev@futureperfect.info>
2014-02-19 19:22:55 +0200

    add drone.sh script

---
+++ drone.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Get package dependencies
+pub get
+
+# Run the readme example
+dart --checked example/readme.dart
+
+# Run the tests
+dart --checked test/undone_test.dart
+
+# Generate API docs and push to gh-pages
+dartdoc --package-root packages --include-lib undone lib/undone.dart
+git checkout gh-pages
+cd docs/
+cp -r . ..
+cd ../
+git add -A
+git commit -m"auto commit from drone"
+git remote set-url origin git@github.com:rmsmith/undone.git
+git push origin gh-pages
