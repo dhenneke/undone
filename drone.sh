45d98f47adcd81078dbf030411b811625b1d8657
rmsmith <dev@futureperfect.info>
2014-03-05 09:56:10 +0200

    drone.sh : first attempt to add `docgen` step

--- drone.sh
+++ drone.sh
@@ -20,3 +20,16 @@ dart --checked test/undone_test.dart
 #git commit -m"auto commit from drone"
 #git remote set-url origin git@github.com:rmsmith/undone.git
 #git push origin gh-pages
+
+docgen --compile --package-root packages --no-include-sdk --no-include-dependent-packages lib/undone.dart
+rm -r packages/
+mkdir packages
+git checkout gh-pages
+cd dartdoc-viewer/client/out/web/
+rsync -rv --exclude=packages . ../../../..
+rsync -rv --exclude=*.dart ../packages ../../../..
+cd ../../../../
+git add -A
+git diff-index --quiet HEAD || git commit -m"auto commit from drone"
+git remote set-url origin git@github.com:rmsmith/undone.git
+git push origin gh-pages
